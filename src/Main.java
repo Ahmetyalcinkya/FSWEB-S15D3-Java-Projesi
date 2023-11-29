d70bc0a177f8a39e12e8a8cbc2eceade504d9b29
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2023-11-29 15:49:38 +0300

    Completed!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
+import com.workintech.maps.Employee;
+
+import java.util.*;
+
 public class Main {
+
+    public static void employeeMaps() {
+
+        Employee employee1 = new Employee(46532168, "Doğancan ", "Kınık");
+        Employee employee2 = new Employee(87153289, "Emre ", "Şahiner");
+        Employee employee3 = new Employee(94652356, "Erhan ", "Fırat");
+        Employee employee4 = new Employee(98465231, "Gökhan ", "Özdemir");
+        Employee employee5 = new Employee(13968435, "Ahmet Can ", "Yalçınkaya");
+
+        List<Employee> employees = new LinkedList<>();
+        List<Employee> duplicates = new LinkedList<>();
+        Map<Integer, String> unique = new HashMap<>();
+
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee2);
+        employees.add(employee2);
+        employees.add(employee3);
+        employees.add(employee3);
+        employees.add(employee4);
+        employees.add(employee4);
+        employees.add(employee5);
+        employees.add(employee5);
+        employees.add(employee5);
+
+        System.out.println(employees);
+        System.out.println("----------------------------");
+
+        Iterator<Employee> employeeIterator = employees.iterator();
+        while (employeeIterator.hasNext()) {
+
+            Employee employee = employeeIterator.next();
+            if (unique.containsKey(employee.getId())) {
+                duplicates.add(employee);
+                employeeIterator.remove();
+            } else {
+                unique.put(employee.getId(), (employee.getFirstName() + employee.getLastName()));
+            }
+        }
+
+        System.out.println(employees);
+
+        System.out.println("----------------------------");
+        System.out.println("Original Employee List:");
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+        System.out.println("\nDuplicates List:");
+        for (Employee employee : duplicates) {
+            System.out.println(employee);
+        }
+
+        System.out.println("\nUnique Employee Map:");
+        for (Map.Entry<Integer, String> entry : unique.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        employeeMaps();
+
     }
 }
---
+++ src/com/workintech/maps/Employee.java
@@ -0,0 +1,54 @@
+package com.workintech.maps;
+
+import java.util.Objects;
+
+public class Employee implements Comparable<Employee> {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Employee(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        return firstName.compareTo(o.firstName);
+    }
+}
